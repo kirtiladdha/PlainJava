d621fc44b02988f7369857bd2210e7cba16763cc
kirtiladdha <122772948+kirtiladdha@users.noreply.github.com>
2023-02-08 19:25:15 +0000

    Unit Test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,7 @@
 package com.tomgregory;
 
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;`
 public class CalculatorTest {
     @Test
     public void canRunApplication() {
